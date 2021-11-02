f2ef9ed57b70565830b5a446886d2a6a978d05dd
unknown <32152412@dankook.ac.kr>
2021-11-02 13:25:29 +0900

    state component ver 2 class type

--- step2/main.js
+++ step2/main.js
@@ -1,24 +1,43 @@
-const $app = document.querySelector('#app');
-
-let state = {
-    items: ['item1', 'item2', 'item3', 'item4']
-}
-
-const render = () => {
-    const { items } = state;
-    $app.innerHTML= `
-    <ul>
-        ${items.map(item => `<li>${ item }</li>`).join('')}
-    </ul>
-    <button id="append">추가</button>
-    `;
-    document.querySelector('#append').addEventListener('click', ()=> {
-        setState({ items: [...items, `item${items.length + 1}` ]})
-    })
-}
-
-const setState = (newState) => {
-    state = { ...state, ...newState };
-    render();
-}
-render();
+class Component {
+    $target;
+    $state;
+    constructor ($target) { 
+      this.$target = $target;
+      this.setup();
+      this.render();
+    }
+    setup () {};
+    template () { return ''; }
+    render () {
+      this.$target.innerHTML = this.template();
+      this.setEvent();
+    }
+    setEvent () {}
+    setState (newState) {
+      this.$state = { ...this.$state, ...newState };
+      this.render();
+    }
+  }
+  
+  class App extends Component {
+    setup () {
+      this.$state = { items: ['item1', 'item2'] };
+    }
+    template () {
+      const { items } = this.$state;
+      return `
+          <ul>
+            ${items.map(item => `<li>${item}</li>`).join('')}
+          </ul>
+          <button>추가</button>
+      `
+    }
+    
+    setEvent () {
+      this.$target.querySelector('button').addEventListener('click', () => {
+        const { items } = this.$state;
+        this.setState({ items: [ ...items, `item${items.length + 1}` ] });
+      }); 
+    }
+  }
+new App(document.querySelector('#App'));
