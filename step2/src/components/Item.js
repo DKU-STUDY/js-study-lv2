460f985e74ce487d1a0ef2473044cb29f1cb59d2
unknown <32152412@dankook.ac.kr>
2021-11-03 11:23:52 +0900

    add delete event

--- step2/src/components/Item.js
+++ step2/src/components/Item.js
@@ -8,15 +8,28 @@ export default class Items extends components {
         const { items } = this.$state;
         return`
         <ul>
-            ${items.map(item => `<li>${item}</li>`).join('')}
+            ${items.map((item, key) =>`
+                <li>
+                    ${item}
+                    <button class="deleteBtn" data-index="${key}">삭제</button>
+                </li>
+                `).join('')}
         </ul>
-        <button>추가</button>   
+        <button class="addBtn">추가</button>   
         `
     }
     setEvent () {
-        this.$target.querySelector('button').addEventListener('click', ()=>{
+        this.$target.querySelector('.addBtn').addEventListener('click', ()=>{
         const { items } = this.$state;
         this.setState({ items: [ ...items, `item${items.length + 1}`]});
+        });
+
+        this.$target.querySelectorAll('.deleteBtn').forEach(deleteBtn => {
+            deleteBtn.addEventListener('click', ({ target }) => {
+                const items = [ ...this.$state.items];
+                items.splice(target.dataset.index, 1);
+                this.setState({ items });
+            })        
         });
     }
 }
